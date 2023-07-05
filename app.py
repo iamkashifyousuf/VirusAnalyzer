e27e9729cd288bd0a053bb9526d59d3f1a2dfc9a
volkanakyuz <volkanakyz0@gmail.com>
2023-07-05 18:15:53 +0300

    API REquest

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
 import requests
-from flask import Flask, request, redirect, url_for, render_template, jsonify
+from flask import Flask, request, redirect, url_for, render_template
+
 import vt
 
 # Create a Flask application
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
-# client = vt.Client("af11c1b89d0dd596ac59e6a138ee33dfe719e87fbf0394f87863a49d7aad0465")
-
-# file = client.get_object("img.png")
 
 url = "https://www.virustotal.com/api/v3/files"
-files = {"file": ("img.png", open("img.png", "rb"), "image/png")}
+
+files = { "file": ("img.png", open("img.png", "rb"), "image/png") }
 headers = {
     "accept": "application/json",
-    "x-apikey": "af11c1b89d0dd596ac59e6a138ee33dfe719e87fbf0394f87863a49d7aad0465",
+    "x-apikey": "af11c1b89d0dd596ac59e6a138ee33dfe719e87fbf0394f87863a49d7aad0465"
 }
-response = requests.post(url, files=files, headers=headers)
-print(response.text)
-print(jsonify(response))
 
+response = requests.post(url, files=files, headers=headers)
+
+print(response.text)
 
 @app.route('/')
 def index():
